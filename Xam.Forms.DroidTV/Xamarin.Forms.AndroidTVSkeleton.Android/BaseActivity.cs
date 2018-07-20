ced0b59ada1c0165abe48a649e911322893db15b
MarkZither <mark.burton@zither-it.co.uk>
2018-07-20 18:54:34 +0200

    add base activity to support voice control

---
+++ Xam.Forms.DroidTV/Xamarin.Forms.AndroidTVSkeleton.Android/BaseActivity.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Provider;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+
+namespace Xamarin.Forms.AndroidTVSkeleton.Droid
+{
+    /// <summary>
+    /// https://github.com/jamesmontemagno/MarshmallowSamples/tree/master/VoiceInteractions/VoiceCamera
+    /// </summary>
+    [Activity(Label = "Voice Camera", LaunchMode = LaunchMode.SingleTop)]
+    [IntentFilter(new[] { MediaStore.IntentActionStillImageCamera }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryVoice })]
+    [IntentFilter(new[] { MediaStore.IntentActionStillImageCameraSecure }, Categories = new[] { Intent.CategoryDefault, Intent.CategoryVoice })]
+
+    public abstract class BaseActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        public Toolbar Toolbar
+        {
+            get;
+            set;
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(LayoutResource);
+            Toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            if (Toolbar != null)
+            {
+                SetSupportActionBar(Toolbar);
+                SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+                SupportActionBar.SetHomeButtonEnabled(true);
+
+            }
+        }
+
+        protected abstract int LayoutResource
+        {
+            get;
+        }
+
+        protected int ActionBarIcon
+        {
+            set { Toolbar.SetNavigationIcon(value); }
+        }
+    }
+}
--- Xam.Forms.DroidTV/Xamarin.Forms.AndroidTVSkeleton.Android/MainActivity.cs
+++ Xam.Forms.DroidTV/Xamarin.Forms.AndroidTVSkeleton.Android/MainActivity.cs
@@ -6,14 +6,29 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android;
 
 namespace Xamarin.Forms.AndroidTVSkeleton.Droid
 {
 
     [Activity(Label = "Xamarin.Forms.AndroidTVSkeleton", Icon = "@mipmap/icon", Theme = "@style/MainTheme", 
         MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : BaseActivity
     {
+        readonly string[] Permissions =
+            {
+                Manifest.Permission.Camera,
+                Manifest.Permission.WriteExternalStorage
+            };
+
+        protected override int LayoutResource
+        {
+            get
+            {
+                return 0;// Resource.Layout.main; 
+            }
+        }
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
